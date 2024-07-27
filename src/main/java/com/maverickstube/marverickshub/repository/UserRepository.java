3f3907a917b0ae3bb8bcf9237c9f0e378dde8465
philip <ajibolaphilip10@gmail.com>
2024-07-27 01:46:46 +0100

    some changes were made

--- src/main/java/com/maverickstube/marverickshub/repository/UserRepository.java
+++ src/main/java/com/maverickstube/marverickshub/repository/UserRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     @Query("select u from User u where u.email=:email")
-    Optional<User> findByEmail(String email);
+   Optional<User> findByEmail(String email);
 }
