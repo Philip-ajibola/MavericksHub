eeebbcab9448a5cca2d28e51fbddb43f6a660c68
philip <ajibolaphilip10@gmail.com>
2024-07-27 01:46:14 +0100

    some changes were made

---
+++ src/main/java/com/maverickstube/marverickshub/dtos/request/Sender.java
@@ -0,0 +1,11 @@
+package com.maverickstube.marverickshub.dtos.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+public class Sender {
+    private final String name = "mavericks hub";
+    private final String email = "noreply@mavericks.com";
+}
