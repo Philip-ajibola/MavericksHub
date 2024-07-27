0dffcb82778c421ae9f14b5c3f2359727545cab9
philip <ajibolaphilip10@gmail.com>
2024-07-27 01:40:09 +0100

    some changes were made

---
+++ src/main/java/com/maverickstube/marverickshub/dtos/response/BrevoMailResponse.java
@@ -0,0 +1,10 @@
+package com.maverickstube.marverickshub.dtos.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class BrevoMailResponse {
+    private String messageId;
+}
