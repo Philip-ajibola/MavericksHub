48b7e74124a16d7694479aa4d73d4f1ae3a22b05
philip <ajibolaphilip10@gmail.com>
2024-07-27 01:44:19 +0100

    some changes were made

--- src/main/java/com/maverickstube/marverickshub/services/MavericksHubUserService.java
+++ src/main/java/com/maverickstube/marverickshub/services/MavericksHubUserService.java
@@ -4,11 +4,15 @@ import com.cloudinary.utils.ObjectUtils;
 import com.maverickstube.marverickshub.dtos.request.CreateUserRequest;
 import com.maverickstube.marverickshub.dtos.response.CreateUserResponse;
 import com.maverickstube.marverickshub.exception.UserNotFoundException;
+import com.maverickstube.marverickshub.models.Authority;
 import com.maverickstube.marverickshub.models.User;
 import com.maverickstube.marverickshub.repository.UserRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
 
 
 @Service
@@ -17,14 +21,20 @@ public class MavericksHubUserService implements UserService {
     private UserRepository userRepository;
     @Autowired
     private final ModelMapper modelMapper;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
 
-    public MavericksHubUserService(ModelMapper modelMapper) {
+    public MavericksHubUserService(ModelMapper modelMapper,PasswordEncoder passwordEncoder) {
         this.modelMapper = modelMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public CreateUserResponse register(CreateUserRequest request) {
        User user =  modelMapper.map(request, User.class);
+       user.setPassword(passwordEncoder.encode(user.getPassword()));
+       user.setAuthorities(new HashSet<>());
+       user.getAuthorities().add(Authority.USER);
        user = userRepository.save(user);
        var response = modelMapper.map(user,CreateUserResponse.class);
        response.setMessage("user registered successfully");
